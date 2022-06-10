cd78f278b105bd47e4aaced513fa57a690541c9b
Pack <zacharypawloski@gmail.com>
2022-06-09 20:58:24 -0400

    added IDs to factButton and pictureButton, linked javascript,  as well as a console log button test for the picture button to make sure it's linked correctly

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var pictureButton = document.getElementById("pictureButton");
+
+var factButton = document.getElementById("factButton");
+
+function buttonTest(){
+    console.log("is this working?");
+}
+pictureButton.addEventListener( "click" , buttonTest );
