923ea4080d1beb6645d4bd9f675765da6e10a26a
Betsy <betsy.griffith12@gmail.com>
2022-06-09 21:09:15 -0400

    the tags are showing up, now just to add the pictures and append to the page

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//variable for placing the cat image API into the html
+var catImages = document.querySelector(".cat-image");
+
+//fetching the cat image API "CATAAS"
+var catTags = "https://cataas.com/api/tags"
+fetch(catTags);
+console.log(catTags);
