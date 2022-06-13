d1c9ab2766332cf069c84943b96abc1cf2961379
Betsy <betsy.griffith12@gmail.com>
2022-06-13 10:45:00 -0400

    added input field to html as well as doctype to html and footer to html. Also started working on JS autocomplete code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,61 @@
-<<<<<<< HEAD
 //variable for placing the cat image API into the html
-var catImages = document.querySelector(".cat-image");
-
-//fetching the cat image API "CATAAS"
-var catTags = "https://cataas.com/api/tags"
-fetch(catTags);
-console.log(catTags);
-=======
 var pictureButton = document.getElementById("pictureButton");
-
 var factButton = document.getElementById("factButton");
 
 function buttonTest(){
     console.log("is this working?");
 }
 pictureButton.addEventListener( "click" , buttonTest ); 
-  14  
-index.html
->>>>>>> main
+
+
+//fetching the cat image API "CATAAS"
+var catTags = "https://cataas.com/api/tags"
+    fetch(catTags)
+    .then(function(response){
+    response.json().then(function(data){
+        // console.log(data[1]);
+        for(i = 0; i < data.length; i++){
+            // console.log(data[i]);
+            $(function() {
+                var availableTags = [data]
+                $( "#tags" ).autocomplete({
+                    source: availableTags
+                });
+            })
+        }
+    })
+})
+
+//adding tags to the autocomplete field
+// $( function() {
+//     var availableTags = [
+//       "#Christmas Cat",
+//       "Curious",
+//       "Asp",
+//       "BASIC",
+//       "C",
+//       "C++",
+//       "Clojure",
+//       "COBOL",
+//       "ColdFusion",
+//       "Erlang",
+//       "Fortran",
+//       "Groovy",
+//       "Haskell",
+//       "Java",
+//       "JavaScript",
+//       "Lisp",
+//       "Perl",
+//       "PHP",
+//       "Python",
+//       "Ruby",
+//       "Scala",
+//       "Scheme"
+//     ];
+//     $( "#tags" ).autocomplete({
+//       source: availableTags
+//     });
+//   } );
+
+
+
