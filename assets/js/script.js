615c32b74a0f1f92a0d240dc3dd35410bf0dc9a0
Betsy <betsy.griffith12@gmail.com>
2022-06-14 11:42:26 -0400

    caption is saved to local storage when the button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
+//variables
+var inputValue = document.querySelector("#tags");
+var caption = [];
+var captionEl = document.querySelector(".cat-caption");
+
+
+//functions to save caption to local storage
+function saveCaption(){
+    var caption = inputValue.value.trim()
+    if(!caption){
+        return false;
+    }
+    addCaption(caption)
+};
+
+function addCaption(caption){
+    localStorage.setItem("caption", JSON.stringify(caption));
+}
+
+// //display the caption on the page
+// function displayCaption(){
+//     caption = JSON.parse(localStorage.getItem("caption"));
+//         var displayTag = document.createElement("li");
+//         displayTag.innerHTML = caption;
+//         captionEl.appendChild(displayTag);
+// }
+
+//function to fetch a cat fact
 function getCatFact(){
     var apiUrl="https://meowfacts.herokuapp.com";
     fetch(apiUrl).then(function(response){
@@ -10,6 +38,7 @@ function getCatFact(){
     });
 }
 
+//function to fetch a cat image
 function getCatPic(){
     var apiUrl="https://cataas.com/cat?json=true";
     fetch(apiUrl).then(function(response){
@@ -22,6 +51,7 @@ function getCatPic(){
     });
 }
 
+//Event Listeners
 $("#factButton").click(function(){
     getCatFact();
 });
@@ -30,22 +60,28 @@ $("#pictureButton").click(function(){
    getCatPic();
 });
 
+
+$("#caption-button").click(function(){
+    saveCaption();
+ });
+
 //fetching the cat image API "CATAAS"
-var catTags = "https://cataas.com/api/tags"
-    fetch(catTags)
-    .then(function(response){
-    response.json().then(function(data){
-        for(i = 0; i < data.length; i++){
-            //function to add data to the autocomplete input field
-            $(function() {
-                var availableTags = data
-                $( "#tags" ).autocomplete({
-                    source: availableTags
-                });
-            })
-        }
-    })
-})
+// var catTags = "https://cataas.com/api/tags"
+//     fetch(catTags)
+//     .then(function(response){
+//     response.json().then(function(data){
+//         console.log(data)
+//         for(i = 0; i < data.length; i++){
+//             //function to add data to the autocomplete input field
+//             $(function() {
+//                 var availableTags = data
+//                 $( "#tags" ).autocomplete({
+//                     source: availableTags
+//                 });
+//             })
+//         }
+//     })
+// })
 
 
 
